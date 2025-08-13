b6417726a1ae5569472d8f8b6c0f8ce5e10c4791
nataly <19nat.9295@gmail.com>
2025-08-13 01:47:02 -0500

    Primer commit en develop

---
+++ backend/alembic/env.py
@@ -0,0 +1,70 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+import sys
+import os
+
+# Agrega el directorio raíz del proyecto al path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.db.base import Base
+from app.db.database import MARIADB_URL
+from app.models import (
+    rol,
+    usuario,
+    categoria,
+    motivacion,
+    notadiario,
+    tecnicaafrontamiento,
+    promesa,
+    fallos,
+    tipo_emocion
+)
+
+# Carga la configuración de Alembic
+config = context.config
+
+# Configuración de logging
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# Metadatos de los modelos para autogenerar migraciones
+target_metadata = Base.metadata
+
+# Asegurar que Alembic use la URL de conexión de nuestra app
+config.set_main_option("sqlalchemy.url", MARIADB_URL)
+
+
+def run_migrations_offline() -> None:
+    """Ejecutar migraciones en modo offline."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Ejecutar migraciones en modo online."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
