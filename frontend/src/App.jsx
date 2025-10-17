17cd799a9faadbc746c500d049ce48e1a3ce5305
Jhosep-David <moto.jdsc@gmail.com>
2025-10-16 19:48:37 -0500

    adjunto la proteccion de rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,9 @@ import Login from "./InicioSesion/InicioSesion";
 import Register from "./Registro/Registro";
 import LandingPage from "./LandingPage/LandingPage";
 import Admin from "./Admin/pages/Admin";
-import RecuperarContraseña from "./InicioSesion/RecuperarContraseña"; // Importa el componente tempralmente para provar  se tiene que quitar
-import  RegistroExitoso from "./Registro/RegistroExitoso"; // Importa el componente tempralmente para provar  se tiene que quitar
+import RecuperarContraseña from "./InicioSesion/RecuperarContraseña";
+import RegistroExitoso from "./Registro/RegistroExitoso";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function App() {
   return (
@@ -14,22 +15,32 @@ export default function App() {
       <Routes>
         {/* Redirección automática de / a /home */}
         <Route path="/" element={<Navigate to="/home" replace />} />
-        
-        {/* Landing Page */}
+
+        {/* Páginas públicas */}
         <Route path="/home" element={<LandingPage />} />
-        
-        {/* Login y Registro */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/ver-recuperar" element={<RecuperarContraseña />} />
+        <Route path="/ver-registroexitoso" element={<RegistroExitoso />} />
 
-         <Route path="/ver-recuperar" element={<RecuperarContraseña />} />
-         <Route path="/ver-registroexitoso" element={<RegistroExitoso />} />
-        
-        {/* Admin */}
-        <Route path="/admin" element={<Admin />} />
-        
-        {/* Layout Joven con todas sus rutas internas */}
-        <Route path="/joven/*" element={<JovenLayout />} />
+        {/* 🔒 Rutas protegidas */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/joven/*"
+          element={
+            <ProtectedRoute>
+              <JovenLayout />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
